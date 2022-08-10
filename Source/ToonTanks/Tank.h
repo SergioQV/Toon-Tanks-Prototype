65aff3a2fddf561883c7e3dd688843e75d849a2f
SergioQV <sergiolordi@hotmail.es>
2022-08-10 15:15:38 +0200

    Added Source code

---
+++ Source/ToonTanks/Tank.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tank.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATank : public ABasePawn
+{
+	GENERATED_BODY()
+
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float Speed = 200.f;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	float TurnRate = 45.f;
+
+	void Move(float value);
+	void Turn(float value);
+
+	APlayerController* TankPlayerController;
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+	bool bAlive = true;
+
+	ATank();
+
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	APlayerController* GetTankPlayerController() const {return TankPlayerController;}
+
+
+	void HandleDestruction();
+
+};
